16efdd4731dbd3a47f6d37bb14f2f386c43f99f9
Jamie Bowley <jpbowley@gmail.com>
2020-05-07 08:03:20 +0100

    fix types and bump version

--- src/styled/createStyled.ts
+++ src/styled/createStyled.ts
@@ -2,9 +2,14 @@ import { createElement, Element } from "@bikeshaving/crank";
 import { Tag } from "./tags";
 import { css, cx } from "emotion";
 
+export type CreateElementProps = {
+  class?: string;
+  children?: string | Element | Element[];
+};
+
 export type CreateElement = <T>(
   styles: TemplateStringsArray
-) => (props: { class?: string, children?: string | Element } & Omit<Partial<T>, "children">) => Element;
+) => (props: CreateElementProps & Omit<Partial<T>, "children">) => Element;
 
 export type CreateStyled = (tag: Tag) => CreateElement;
 
